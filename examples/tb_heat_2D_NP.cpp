930a4a612337dfb63abd7031ce2527bad989d275
Charith Paranaliyanage <paranaliyanage@gmail.com>
2014-02-27 10:16:36 +0530

    extract the minimum time instead of average

--- examples/tb_heat_2D_NP.cpp
+++ examples/tb_heat_2D_NP.cpp
@@ -57,6 +57,7 @@ int main(int argc, char * argv[])
 	const int BASE = 1024;
 	int t;
 	struct timeval start, end;
+    double min_tdiff = INF;
     int N_SIZE = 0, T_SIZE = 0;
 
     if (argc < 3) {
@@ -83,13 +84,13 @@ int main(int argc, char * argv[])
         a(1, i, j) = 0;
 	} }
 
-	gettimeofday(&start, 0);
-	for (int times = 0; times < TIMES; ++times) {
-		heat_2D.Run(T_SIZE, heat_2D_fn);
-	}
-	gettimeofday(&end, 0);
-	std::cout << "Pochoir ET: consumed time : "
-			<< 1.0e3 * tdiff(&end, &start) / TIMES << " ms" << std::endl;
+    for (int times = 0; times < TIMES; ++times) {
+	    gettimeofday(&start, 0);
+        heat_2D.Run(T_SIZE, heat_2D_fn);
+	    gettimeofday(&end, 0);
+        min_tdiff = min(min_tdiff, (1.0e3 * tdiff(&end, &start)));
+    }
+	std::cout << "Pochoir ET: consumed time : " << min_tdiff << " ms" << std::endl;
 
 #if 0
     min_tdiff = INF;
